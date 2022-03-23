179621faf24e2d54d0831933ed507f59f448aa06
Reksa Panduasa <panduasareksa@students.amikom.ac.id>
2022-03-23 19:46:25 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tugas_ClassObject_4352_Reksa_Panduasa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat objek taxi
+            Taxi taxi = new Taxi();
+
+            //Pengesetan Nilai Properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //Pemanggilan Method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tugas_ClassObject_4352_Reksa_Panduasa
+{
+    public class Taxi
+    {
+        //properties
+        public string DriverName { get; set; }
+        public bool OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        //method
+        public string OnDutyChoices()
+        {
+            return OnDuty ? "Yes" : "No";
+        }
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDutyChoices());
+            Console.WriteLine("Number Of Passenger : {0}", NumPassenger);          
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n{0} Sedang Mengantar Penumpang", DriverName);
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantar Penumpang", DriverName);
+        }
+    }
+}
